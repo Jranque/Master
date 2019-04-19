a8837af7db730288762941aa7641625e26b1d242
Jranque <Jranque@gmail.com>
2019-04-19 18:52:59 +0800

    creating CRUD using axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  getAllEmployees() {
+    try {
+      axios({
+        method: "get",
+        url: "http://dummy.restapiexample.com/api/v1/employees"
+      }).then(response => {
+        console.log("All Employee Details");
+        console.log(response.data);
+      });
+    } catch (err) {
+      console.log("Error on fetching all employee details");
+      console.log(err);
+    }
+  }
+  createEmployees(details) {
+    try {
+      axios({
+        method: "post",
+        url: "http://dummy.restapiexample.com/api/v1/create",
+        data: details
+      }).then(response => {
+        console.log("All Employee Details");
+        console.log(response.data);
+      });
+    } catch (err) {
+      console.log("Error on fetching all employee details");
+      console.log(err);
+    }
+  }
+  updateEmployees(id, details) {
+    try {
+      axios({
+        method: "put",
+        url: `http://dummy.restapiexample.com/api/v1/update/${id}`,
+        data: details
+      }).then(response => {
+        console.log("All Employee Details");
+        console.log(response.data);
+      });
+    } catch (err) {
+      console.log("Error on fetching all employee details");
+      console.log(err);
+    }
+  }
+  deleteEmployees(id) {
+    try {
+      axios({
+        method: "delete",
+        url: `http://dummy.restapiexample.com/api/v1/delete/${id}`
+      }).then(response => {
+        console.log("All Employee Details");
+        console.log(response.data);
+      });
+    } catch (err) {
+      console.log("Error on fetching all employee details");
+      console.log(err);
+    }
+  }
+
   render() {
+    var details = {
+      name: "boss jen",
+      salary: "32000",
+      age: "100"
+    };
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            onClick={() => {
+              //alert("I am clicked");//
+              //this.getAllEmployees();//
+              //this.createAllEmployees();
+              //this.updateEmployees("435", details);
+              this.deleteEmployees("434", details);
+            }}
           >
-            Learn React
-          </a>
+            click me
+          </button>
         </header>
       </div>
     );
